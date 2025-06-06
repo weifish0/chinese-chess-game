c7cd6e1cfe3a1ac114a7559049ad17c32bfdfdea
weifish0 <will060710@gmail.com>
2025-06-06 18:03:27 +0800

    Add start_scene and basic engine, UI, Allegro files, CMakeLists, Makefile

---
+++ Scene/start_scene.hpp
@@ -0,0 +1,16 @@
+#ifndef STARTSCENE_HPP
+#define STARTSCENE_HPP
+
+#include <allegro5/allegro_audio.h>
+#include <memory>
+
+#include "Engine/IScene.hpp"
+class StartScene final : public Engine::IScene {
+public:
+    explicit StartScene() = default;
+    void Initialize() override;
+    void Terminate() override;
+    void PlayOnClick(int stage);
+    void SettingsOnClick(int stage);
+};
+#endif
