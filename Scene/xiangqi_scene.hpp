0e65c8c76039e1a3bba69b6279ac8d4ba41f1421
FaaberlingOstid <heiliosgp.sih@gmail.com>
2025-06-06 17:17:33 +0800

    Add: several scene files

---
+++ Scene/xiangqi_scene.hpp
@@ -0,0 +1,92 @@
+#ifndef XIANGQISCENE_HPP
+#define XIANGQISCENE_HPP
+#include <allegro5/allegro_audio.h>
+#include <map>
+#include <list>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "Engine/IScene.hpp"
+#include "Engine/Point.hpp"
+
+// For others to use TurretMap.
+typedef std::map<Engine::Point, Turret *> Pos_Turret_Map;
+extern Pos_Turret_Map TurretMap;
+
+class Turret;
+namespace Engine {
+    class Group;
+    class Image;
+    class Label;
+    class Sprite;
+}   // namespace Engine
+
+class PlayScene final : public Engine::IScene {
+private:
+    enum TileType {
+        TILE_DIRT,
+        TILE_FLOOR,
+        TILE_OCCUPIED,
+    };
+    ALLEGRO_SAMPLE_ID bgmId;
+    std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE> deathBGMInstance;
+
+protected:
+    int lives;
+    int money;
+    int SpeedMult;
+
+public:
+    static bool DebugMode;
+    static bool ShovelMode; // Using a shovel.
+    static const std::vector<Engine::Point> directions;
+    static const int MapWidth, MapHeight;
+    static const int BlockSize;
+    static const float DangerTime;
+    static const Engine::Point SpawnGridPoint;
+    static const Engine::Point EndGridPoint;
+    static const std::vector<int> code;
+    int MapId;
+    float ticks;
+    float deathCountDown;
+    // Map tiles.
+    Group *TileMapGroup;
+    Group *GroundEffectGroup;
+    Group *DebugIndicatorGroup;
+    Group *BulletGroup;
+    Group *TowerGroup;
+    Group *EnemyGroup;
+    Group *EffectGroup;
+    Group *UIGroup;
+    Engine::Label *UIMoney;
+    Engine::Label *UILives;
+    Engine::Image *imgTarget;
+    Engine::Sprite *dangerIndicator;
+    Turret *preview;
+    std::vector<std::vector<TileType>> mapState;
+    std::vector<std::vector<int>> mapDistance;
+    std::list<std::pair<int, float>> enemyWaveData;
+    std::list<int> keyStrokes;
+    static Engine::Point GetClientSize();
+    explicit PlayScene() = default;
+    void Initialize() override;
+    void Terminate() override;
+    void Update(float deltaTime) override;
+    void Draw() const override;
+    void OnMouseDown(int button, int mx, int my) override;
+    void OnMouseMove(int mx, int my) override;
+    void OnMouseUp(int button, int mx, int my) override;
+    void OnKeyDown(int keyCode) override;
+    void Hit();
+    int GetMoney() const;
+    void EarnMoney(int money);
+    void ReadMap();
+    void ReadEnemyWave();
+    void ConstructUI();
+    void UIBtnClicked(int id);
+    bool CheckSpaceValid(int x, int y);
+    std::vector<std::vector<int>> CalculateBFSDistance();
+    // void ModifyReadMapTiles();
+};
+#endif   // XIANGQISCENE_HPP
